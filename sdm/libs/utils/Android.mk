7d8c7b4057dd0f1304ef2376117bfb2048e1f669
Adarsh-MR <adarshmr1998@gmail.com>
2018-07-26 18:37:30 +0000

    display: move more modules to vendor
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- sdm/libs/utils/Android.mk
+++ sdm/libs/utils/Android.mk
@@ -2,6 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE                  := libsdmutils
+LOCAL_VENDOR_MODULE           := true
 LOCAL_MODULE_TAGS             := optional
 LOCAL_C_INCLUDES              := $(call project-path-for,qcom-display)/sdm/include/
 LOCAL_CFLAGS                  := -Wno-missing-field-initializers -Wno-unused-parameter \
