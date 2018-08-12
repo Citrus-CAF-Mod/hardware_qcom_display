76eea2c927a819859708aa649b9579f43e338c1c
Vijay Venkatraman <vijaykv@google.com>
2018-08-12 12:07:09 +0000

    msm8952: replacing LOCAL_COPY_HEADERS with LOCAL_EXPORT_C_INCLUDE_DIRS, for VNDK
    Bug: 33241851
    Test: build sailfish targets and pass cts-camera tests
    Change-Id: I384cbc52ec6de502ed978ddad9fffdec2923abc1
    
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- libqdutils/Android.mk
+++ libqdutils/Android.mk
@@ -9,8 +9,7 @@ LOCAL_SHARED_LIBRARIES        := $(common_libs) libui libbinder libqservice
 LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdutils\" -Wno-float-conversion
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
-LOCAL_COPY_HEADERS_TO         := $(common_header_export_path)
-LOCAL_COPY_HEADERS            := mdp_version.h
+LOCAL_EXPORT_C_INCLUDE_DIRS   := $(LOCAL_PATH)
 LOCAL_SRC_FILES               := profiler.cpp mdp_version.cpp \
                                  idle_invalidator.cpp \
                                  comptype.cpp qd_utils.cpp \
@@ -19,8 +18,7 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_COPY_HEADERS_TO           := $(common_header_export_path)
-LOCAL_COPY_HEADERS              := DisplayConfig.h display_config.h
+LOCAL_EXPORT_C_INCLUDE_DIRS     := $(LOCAL_PATH) 
 LOCAL_SHARED_LIBRARIES          := $(common_libs) libbinder libqservice libqdutils
 LOCAL_C_INCLUDES                := $(common_includes)
 LOCAL_ADDITIONAL_DEPENDENCIES   := $(common_deps)
@@ -34,8 +32,7 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_COPY_HEADERS_TO           := $(common_header_export_path)
-LOCAL_COPY_HEADERS              := qdMetaData.h qd_utils.h
+LOCAL_EXPORT_C_INCLUDE_DIRS     := $(LOCAL_PATH)
 LOCAL_SHARED_LIBRARIES          := liblog libcutils
 LOCAL_C_INCLUDES                := $(common_includes)
 LOCAL_ADDITIONAL_DEPENDENCIES   := $(common_deps)
